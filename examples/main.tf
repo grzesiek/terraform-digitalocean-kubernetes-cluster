a324fbc6455922b68682199ba94b850333ab575e
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2020-03-14 12:29:45 +0100

    Add kubeconfig output

--- examples/main.tf
+++ examples/main.tf
@@ -6,3 +6,8 @@ module "digitalocean_cluster" {
 output "cluster_ip" {
   value = module.digitalocean_cluster.cluster_ip
 }
+
+output "kubeconfig" {
+  sensitive = true
+  value     = module.digitalocean_cluster.raw_kubeconfig
+}
